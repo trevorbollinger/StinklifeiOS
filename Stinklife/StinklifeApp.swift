4be50e624058607c1f679db503fdca5922c017cf
trevorbollinger <68718850+trevorbollinger@users.noreply.github.com>
2023-01-28 13:08:10 -0600

    Initial Commit

---
+++ Stinklife/StinklifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StinklifeApp.swift
+//  Stinklife
+//
+//  Created by Trevor Bollinger on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct StinklifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
